820e2ef746241e12c06e092bf53f6ce39489eff2
unknown <admin@BGAI202-009.bga.upb>
2026-04-28 20:54:39 -0500

    Primer commit

---
+++ Tareas.js
@@ -0,0 +1,32 @@
+console.log("-----Bienvenido a la plataforma de tareas-----");
+
+function crearTarea() {
+  const tareaNueva = []
+  const titulo = prompt("Ingrese el título de la tarea:");
+  const descripcion = prompt("Ingrese la descripción de la tarea:");
+  let estado = prompt("Ingrese el estado de la tarea:"); 
+    
+  if (estado == "completada"){
+    console.log(`Tarea terminada: ${titulo} - ${descripcion} `);
+ }
+  else {
+    console.log("Tarea en proceso o no existente.");
+ }
+  tareaNueva.push(crearTarea);
+  function filtrarTarea(){
+  const filtrar = tareaNueva.filter()
+}
+  
+}
+
+
+let continuar = prompt("Quieres agregar otra tarea?:");
+while (continuar){
+  if (continuar == "si"){
+    crearTarea();
+  }
+  else{
+    console.log("Saliendo");
+    break
+  }
+}
